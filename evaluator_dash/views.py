c5013b42ef4a776f82977e35466efeca7388e08f
Dumajal Malla <127682737+dumajal@users.noreply.github.com>
2026-04-05 23:50:26 +0545

    Update views.py

--- evaluator_dash/views.py
+++ evaluator_dash/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from student_dash.models import Thesis
+from .forms import EvaluationForm
+from .models import Evaluation
 
-# Create your views here.
+# list of theses
+def thesis_list(request):
+    theses = Thesis.objects.all()
+    return render(request,
+                  'evaluator_dash/thesis_list.html',
+                  {'theses': theses}
+                  )
+
+# thesis details
+def thesis_detail(request, id):
+    thesis = get_object_or_404(Thesis, id=id)
+    evaluations = Evaluation.objects.filter(thesis=thesis)
+    return render(request,
+                  'evaluator_dash/thesis_detail.html',
+                  {
+                      'thesis': thesis,
+                      'evaluations': evaluations
+                  })
+  
+# evaluate thesis
+def evaluate_thesis(request, id):
+    thesis = get_object_or_404(Thesis, id=id)
+    form = EvaluationForm()
+    if request.method == "POST":
+        form = EvaluationForm(request.POST)
+        if form.is_valid():
+            evaluation = form.save(commit=False)
+            evaluation.thesis = thesis
+            # temporary evaluator (can replace with logged user later)
+            evaluation.evaluator_id = 1
+            evaluation.save()
+            return redirect('thesis_detail', id=id)
+    return render(request,
+                  'evaluator_dash/evaluate.html',
+                  {
+                      'form': form,
+                      'thesis': thesis
+                  })
