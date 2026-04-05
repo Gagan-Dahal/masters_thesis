1c9c7517acac2ab517ff44238632040dba436ed5
Dumajal Malla <127682737+dumajal@users.noreply.github.com>
2026-04-05 23:47:11 +0545

    Add Evaluation model with status and relationships

--- evaluator_dash/models.py
+++ evaluator_dash/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from department.models import Instructor
+from student_dash.models import Thesis
 
-# Create your models here.
+class Evaluation(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Revision', 'Need Revision'),
+        ('Rejected', 'Rejected')
+    ]
+    thesis = models.ForeignKey(Thesis, on_delete=models.CASCADE)
+    evaluator = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    comment = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    evaluation_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.thesis.title
